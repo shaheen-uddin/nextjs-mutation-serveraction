82a3d34f522de4907aa9d744e483b5102b712c54
shaheen uddin <oak@local.debian>
2024-04-16 11:33:17 +0600

    Adding user to mongodb

--- src/app/components/NewUserForm.jsx
+++ src/app/components/NewUserForm.jsx
@@ -1,9 +1,16 @@
+import connectMongo from "../dbConnect/connectMongo";
+import User from "../models/User";
+
 export default function NewUserForm() {
   const addUser = async (formData) => {
     "use server";
-    console.log(formData.get("name"));
-    console.log(formData.get("email"));
+    const name = formData.get("name");
+    const email = formData.get("email");
     //console.log(formData);
+    const userData = { name, email };
+
+    await connectMongo();
+    await new User(userData).save();
   };
   return (
     <form action={addUser} className="flex flex-col gap-3">
